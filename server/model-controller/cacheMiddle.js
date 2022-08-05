ef827cae8127cc4d7dc572b9f1133317341a51a2
Heemo Yang <heemo521@gmail.com>
2022-08-05 14:09:08 -0700

    enable cache again and fix get request

--- server/model-controller/cacheMiddle.js
+++ server/model-controller/cacheMiddle.js
@@ -1,22 +1,17 @@
 const NodeCache = require("node-cache");
 const cache = new NodeCache();
 
-
-module.exports = duration => (req, res, next) => {
-  if (req.method !== 'GET') {
-    console.error('Cannot cache non-GET methods!');
-    return next();
-  }
+module.exports = (duration) => (req, res, next) => {
   const key = req.originalUrl;
   const cachedResponse = cache.get(key);
   if (cachedResponse) {
     return res.status(200).json(cachedResponse);
   } else {
     res.originalSend = res.send;
-    res.send = body => {
+    res.send = (body) => {
       res.originalSend(body);
       cache.set(key, body, duration);
-    }
+    };
     next();
   }
-}
+};
