85b5b96fe61c84a7a92d77a3f5069c586007298e
Heemo Yang <heemo521@gmail.com>
2022-08-04 23:02:36 -0700

    loader

--- server/qna_router/questionsRouter.js
+++ server/qna_router/questionsRouter.js
@@ -1,24 +1,37 @@
-
-const router = require('express').Router();
-const { getQuestions, getAnswers, postQuestion, postAnswer, addQuestionHelpful, reportQuestion } = require('../model-controller').questions;
+const router = require("express").Router();
+const path = require("path");
+const {
+  getQuestions,
+  getAnswers,
+  postQuestion,
+  postAnswer,
+  addQuestionHelpful,
+  reportQuestion,
+} = require("../model-controller").questions;
 // const { getQuestions } = require('../model-controller').questions;
 
 // List Questions
-router.get('', getQuestions)
+router.get("", getQuestions);
+
+router.get("/loaderio-072dcb9324d8a7b71a419894a382eb21/", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, '/server/db/loaderio-072dcb9324d8a7b71a419894a382eb21.txt');
+  );
+});
 
 // Answers List
-router.get('/:question_id/answers', getAnswers)
+router.get("/:question_id/answers", getAnswers);
 
 // Add a Question
-router.post('', postQuestion);
+router.post("", postQuestion);
 
 // Add an Answer
-router.post('/:question_id/answers', postAnswer);
+router.post("/:question_id/answers", postAnswer);
 
 //Mark Question as Helpful
-router.put('/:question_id/helpful', addQuestionHelpful);
+router.put("/:question_id/helpful", addQuestionHelpful);
 
 // Report Question
-router.put('/:question_id/report', reportQuestion);
+router.put("/:question_id/report", reportQuestion);
 
 module.exports = router;
